4c04215484c71c297d5c42ce42f57c4db1339477
kinoshita-rm-learn <kinoshita_rm@ict-co.co.jp>
2020-05-08 16:22:06 +0900

    ブランチ作成

---
+++ branch_test.py
@@ -0,0 +1,17 @@
+# Ğ‰ï•ÛŒ¯—¿—¦
+social_rate = 0.15
+
+# Š“¾Å—¦
+tax_rate = 0.1
+
+# 10‰~’PˆÊlÌŒÜ“ü
+def round10(v):
+r = int(v / 10 + 0.5) * 10
+return r
+
+# 10‰~’PˆÊØ‚èÌ‚Ä
+def trunc10(v):
+    t = int(v / 10) * 10
+    return t
+
+if __name__ == "__main__":
