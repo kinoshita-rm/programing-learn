5a49c430e72777cbdc44eb170d43f6d13133059d
kinoshita-rm-learn <kinoshita_rm@ict-co.co.jp>
2020-05-08 17:04:48 +0900

    b

--- branch_test.py
+++ branch_test.py
@@ -15,3 +15,20 @@ def trunc10(v):
     return t
 
 if __name__ == "__main__":
+# ˆê”ÊĞˆõ
+def calc_general(base, over):
+    over_price = trunc10(base / 160.0 * 1.25)
+    salary = base + over_price * over
+    social = round10(salary * social_rate)
+    tax = (salary - social) * tax_rate
+    bank = salary - social - tax
+    return int(bank)
+
+if __name__ == "__main__":
+    bank_terada = calc_general(320000, 30)
+    bank_hiro = calc_general(295000, 20)
+    bank_suga = calc_general(220000, 35)
+
+    print(f'›“c”¿‚³‚ñ‚ÌUŠz={bank_terada}')
+    print(f'L“cN”‚³‚ñ‚ÌUŠz={bank_hiro}')
+    print(f'›À—mˆê˜Y‚³‚ñ‚ÌUŠz={bank_suga}')
